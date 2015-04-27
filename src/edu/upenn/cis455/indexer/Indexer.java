2517f0d14c99a18f38e7e3ef22ee64ef2d2bac0b
Corey Loman <coreyloman@gmail.com>
2015-04-27 00:27:01 -0400

    we're sort of pulling from s3

--- src/edu/upenn/cis455/indexer/Indexer.java
+++ src/edu/upenn/cis455/indexer/Indexer.java
@@ -1,7 +1,20 @@
 package edu.upenn.cis455.indexer;
 
+import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import edu.upenn.cis455.storage.DBWrapperIndexer;
 
@@ -17,9 +30,14 @@ public class Indexer {
 		start();
 	}
 	
-	public void start() {
+	public static void main(String[] args) {
+		start();
+	}
+	
+	public static void start() {
 		// pulls from S3
 		// adds new url -> docContent to the blocking queue
+		S3Object object = extractObject();
 		
 		//parse what we pulled from S3 here
 		// check to see if it's XML or HTML
@@ -31,6 +49,35 @@ public class Indexer {
 		// put all PR results to database, mapping url to PageRank score
 	}
 	
+	private static S3Object extractObject() {
+		 AWSCredentials credentials = null;
+	        try {
+	            credentials = new ProfileCredentialsProvider("cis455_mark").getCredentials();
+	        } catch (Exception e) {
+	            throw new AmazonClientException(
+	                    "Cannot load the credentials from the credential profiles file. " +
+	                    "Please make sure that your credentials file is at the correct " +
+	                    "location (/home/cis455/.aws/credentials), and is in valid format.",
+	                    e);
+	        }
+
+	        AmazonS3 s3 = new AmazonS3Client(credentials);
+	        Region usWest2 = Region.getRegion(Regions.US_EAST_1);
+	        s3.setRegion(usWest2);
+
+	        String bucketName = "for.indexer";
+	        System.out.println("Listing objects");
+            ObjectListing objectListing = s3.listObjects(new ListObjectsRequest()
+                    .withBucketName(bucketName)
+                    .withPrefix("DocS3batch:"));
+            for (S3ObjectSummary objectSummary : objectListing.getObjectSummaries()) {
+                System.out.println(" - " + objectSummary.getKey() + "  " +
+                                   "(size = " + objectSummary.getSize() + ")");
+            }
+            System.out.println();
+		return null;
+	}
+
 	class DocIndexer extends Thread {
 		
 		public DocIndexer() {
